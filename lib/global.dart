e451b82f44692e83e153f1be52165bc70f12e5d0
ksw2003 <162660075+ksw2003@users.noreply.github.com>
2025-08-31 03:51:28 +0900

    0831 - 필사 페이지

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,8 @@
 library globals;
 
-Map<String, List<Map<String, String>>> savedWritings = {};
-List<Map<String, String>> randomSavedWritings = [];
+Map<String, List<Map<String, String?>>> savedWritings = {};
+Map<String, List<Map<String, String?>>> savedRandomWritings = {};
+List<Map<String, dynamic>> transcribedBooks = [];
+String extractedText = "";
 
 String? selectedRandomTopic;
